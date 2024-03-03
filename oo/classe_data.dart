85861af1471132a7f76efeb1d89e47b2fa93127e
luisgs7 <luisgsilva270@gmail.com>
2024-03-03 11:57:17 -0300

    dart class.

--- oo/classe_data.dart
+++ oo/classe_data.dart
@@ -3,6 +3,21 @@ class Data {
   int? mes;
   int? ano;
 
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+
+  //Named Constructors
+  Data.com({this.dia = 1, this.mes = 1, this.ano = 1970});
+  Data.ultimoDiaDoAno(this.ano) {
+    dia = 3;
+    mes = 12;
+  }
+
   String obterDataFormatada() {
     return "$dia/$mes/$ano";
   }
@@ -14,12 +29,12 @@ class Data {
 }
 
 void main(List<String> args) {
-  var dataAniversario = new Data();
+  var dataAniversario = new Data(2, 2, 3000);
   dataAniversario.dia = 3;
   dataAniversario.mes = 4;
   dataAniversario.ano = 5;
 
-  Data dataCompra = Data();
+  Data dataCompra = Data(3, 4, 5000);
   dataCompra.dia = 01;
   dataCompra.mes = 03;
   dataCompra.ano = 2025;
@@ -29,4 +44,9 @@ void main(List<String> args) {
 
   print(dataAniversario.toString());
   print(dataCompra);
+
+  print(Data());
+
+  print(Data.com());
+  print(Data.ultimoDiaDoAno(2014));
 }
---
+++ oo/test/teste_carro.dart
@@ -0,0 +1,17 @@
+import '../model/carro.dart';
+
+void main() {
+  var c1 = Carro(320);
+
+  while (!c1.estaNoLimite()) {
+    print("Velocidade Atual: ${c1.acelerar()} km/h");
+  }
+
+  print("Chegou a velocidade máxima: ${c1.velocidadeAtual}");
+
+  while (!c1.estaParado()) {
+    print("A velocidade atual é: ${c1.frear()} km/h");
+  }
+
+  print("O carro parou com velocidade: ${c1.velocidadeAtual} km/h");
+}
